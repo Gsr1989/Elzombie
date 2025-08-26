2ead8bbed88ef9a0cbd06fb37dd12afe2afd3742
Gsr1989 <empresalorens@gmail.com>
2025-08-25 20:35:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ PLANTILLA_PDF = "cdmxdigital2025ppp.pdf"
 PLANTILLA_BUENO = "elbueno.pdf"
 
 # Precio del permiso
-PRECIO_PERMISO = EL MISMO DE SIEMPRE 
+PRECIO_PERMISO = EL_MISMO_DE_SIEMPRE 
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
