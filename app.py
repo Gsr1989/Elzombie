12d25ae9a567551b09b019218323d195b5d6b412
Gsr1989 <empresalorens@gmail.com>
2025-09-05 15:25:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -265,10 +265,10 @@ def generar_pdf_principal(datos: dict) -> str:
         qr_pix = fitz.Pixmap(buf.read())
 
         # Coordenadas del QR (ajustar según tu PDF de CDMX)
-        x_qr = 50  
-        y_qr = 300
-        ancho_qr = 90
-        alto_qr = 90
+        x_qr = 47  
+        y_qr = 500
+        ancho_qr = 95
+        alto_qr = 95
 
         page.insert_image(
             fitz.Rect(x_qr, y_qr, x_qr + ancho_qr, y_qr + alto_qr),
