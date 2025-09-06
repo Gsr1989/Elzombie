80da085c27c874a7233c38ff699d03414121ca67
Gsr1989 <empresalorens@gmail.com>
2025-09-05 22:32:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ def obtener_siguiente_folio():
     """
     folio_num = folio_counter["siguiente"]
     folio = f"{FOLIO_PREFIJO}{folio_num}"
-    folio_counter["siguiente"] += 3
+    folio_counter["siguiente"] += 4
     return folio
 
 def inicializar_folio_desde_supabase():
